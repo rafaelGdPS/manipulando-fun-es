460cb19bdb2eb078bfb429ca53b9994c144da314
Rafael <rafaelgermanio@gmail.com>
2023-08-28 17:54:57 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const objectAnimal = (animal) => {
+  const array = data.species.find((specie) => Object.values(animal)[0] === specie.name);
+  if (Object.keys(animal).includes('sex')) {
+    const sexResident = array.residents
+      .filter((resident) => Object.values(animal)[1] === resident.sex);
+    return sexResident.length;
+  }
+  return array.residents.length;
 };
+
+const countAnimals = (animal) => {
+  const obj = {};
+  data.species
+    .forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+    });
+  if (typeof animal === 'object') {
+    return objectAnimal(animal);
+  }
+
+  return obj;
+};
+console.log(countAnimals({ species: 'penguins' }));
 
 module.exports = countAnimals;
